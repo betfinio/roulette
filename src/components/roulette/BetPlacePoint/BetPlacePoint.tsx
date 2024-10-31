6c9c9017c23fe142ca07ad89db3527e521b1a307
bf-san <bf-san@proton.me>
2024-10-31 17:56:35 +0100

    changed hover color. changed tablet removed roulette redundant circle

--- src/components/roulette/BetPlacePoint/BetPlacePoint.tsx
+++ src/components/roulette/BetPlacePoint/BetPlacePoint.tsx
@@ -17,7 +17,7 @@ export const BetPlacePoint: FC<BetPlacePointProps> = ({ positionId, position, ..
 
 	// Mapping positions to Tailwind classes
 	const positionClasses: Record<PositionType, string> = {
-		center: 'top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2',
+		center: 'top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ',
 		top: 'top-[-13px] left-1/2 transform -translate-x-1/2',
 		left: 'top-1/2 -left-[30%] transform -translate-y-1/2 ',
 		right: 'top-1/2 right-[-12px] transform -translate-y-1/2',
@@ -29,14 +29,10 @@ export const BetPlacePoint: FC<BetPlacePointProps> = ({ positionId, position, ..
 	} as const;
 	return (
 		<motion.div
-			className={cn(
-				'absolute w-[40%] max-w-10 aspect-square bg-muted/40 rounded-full flex items-center justify-center z-10 opacity-100',
-				positionClasses[position],
-				{
-					'bg-muted/40': isDebugMode,
-					'bg-transparent': !isDebugMode,
-				},
-			)}
+			className={cn('absolute w-[40%] max-w-10 aspect-square bg-muted/40  flex items-center justify-center z-10 opacity-100', positionClasses[position], {
+				'bg-muted/40': isDebugMode,
+				'bg-transparent': !isDebugMode,
+			})}
 			{...events}
 		>
 			<BetChips positionId={positionId} />
