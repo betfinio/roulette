3eec2e87f4fec274b8bf868a5d8f983be7f43a2e
Monster <bf.monster@proton.me>
2025-02-19 10:19:39 +0100

    refactor: improve roulette live route structure and exports

--- src/routes/games/roulette/live/index.tsx
+++ src/routes/games/roulette/live/index.tsx
@@ -3,10 +3,10 @@ import { createFileRoute, useNavigate } from '@tanstack/react-router';
 import { useEffect } from 'react';
 
 export const Route = createFileRoute('/games/roulette/live/')({
-	component: RouteComponent,
+	component: IndexLiveRoulette,
 });
 
-function RouteComponent() {
+export function IndexLiveRoulette() {
 	const { data: tables = [] } = useGetLiveRouletteTables();
 	const navigate = useNavigate();
 	useEffect(() => {
