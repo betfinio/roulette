0e007fdefad0ea0d28e29fd066eb160b0083116c
BF san <bf-san@proton.me>
2025-05-05 17:04:44 +0200

    fix(#104): fixed issue with slider

--- src/components/shared/BetControls/RangeWithButtons.tsx
+++ src/components/shared/BetControls/RangeWithButtons.tsx
@@ -2,7 +2,7 @@ import { useChangeChip, useSelectedChip } from '@/src/lib/shared/query';
 import { Button } from '@betfinio/components/ui';
 import { Minus, PlusIcon } from 'lucide-react';
 import millify from 'millify';
-import { type FC, useRef, useState } from 'react';
+import type { FC } from 'react';
 import RouletteSlider from '../RouletteSlider/RouletteSlider';
 
 interface IRangeWithButtonsProps {
@@ -14,7 +14,6 @@ export const RangeWithButtons: FC<IRangeWithButtonsProps> = ({ limits }) => {
 	const { data: activeChipValue = 0 } = useSelectedChip();
 
 	const handlePlus = () => {
-		if (activeChipValue > limits.max - limits.min / 100) return;
 		let amount: number;
 		if (activeChipValue < 100_000) {
 			amount = limits.min;
@@ -23,11 +22,13 @@ export const RangeWithButtons: FC<IRangeWithButtonsProps> = ({ limits }) => {
 		} else {
 			amount = 250_000;
 		}
+		if (activeChipValue + amount > limits.max) {
+			amount = limits.max - activeChipValue <= 0 ? 0 : limits.max - activeChipValue;
+		}
 		change({ amount: activeChipValue + amount });
 	};
 
 	const handleMinus = () => {
-		if (activeChipValue < limits.min + limits.max / 100) return;
 		let amount: number;
 		if (activeChipValue <= 100_000) {
 			amount = limits.min;
@@ -36,57 +37,41 @@ export const RangeWithButtons: FC<IRangeWithButtonsProps> = ({ limits }) => {
 		} else {
 			amount = 250_000;
 		}
-		change({ amount: activeChipValue - amount });
-	};
-
-	const [incrementSpeed, setIncrementSpeed] = useState(1);
-	const intervalRef = useRef<NodeJS.Timer | null>(null);
-
-	const startIncrement = (direction: 'increase' | 'decrease') => {
-		if (intervalRef.current) return;
-		intervalRef.current = setInterval(() => {
-			direction === 'increase' ? handlePlus() : handleMinus();
-			setIncrementSpeed((prev) => Math.min(prev * 1.1, 10));
-		}, 200 / incrementSpeed);
-	};
-
-	const stopIncrement = () => {
-		if (intervalRef.current) {
-			clearInterval(intervalRef.current);
-			intervalRef.current = null;
+		if (activeChipValue - amount < limits.min) {
+			amount = activeChipValue - limits.min;
 		}
-		setIncrementSpeed(1);
+		change({ amount: activeChipValue - amount });
 	};
 
 	const marks = [
 		{
 			value: limits.min,
 
-			label: millify(limits.min),
+			label: millify(limits.min, { precision: 2 }),
 		},
 
 		{
 			value: limits.max / 4,
-			label: millify(limits.max / 4),
+			label: millify(limits.max / 4, { precision: 2 }),
 		},
 		{
 			value: (limits.max / 4) * 2,
-			label: millify((limits.max / 4) * 2),
+			label: millify((limits.max / 4) * 2, { precision: 2 }),
 		},
 		{
 			value: (limits.max / 4) * 3,
-			label: millify((limits.max / 4) * 3),
+			label: millify((limits.max / 4) * 3, { precision: 2 }),
 		},
 
 		{
 			value: limits.max,
-			label: millify(limits.max),
+			label: millify(limits.max, { precision: 2 }),
 		},
 	];
 
 	return (
 		<>
-			<Button variant="secondary" onMouseDown={() => startIncrement('decrease')} onMouseUp={stopIncrement} onMouseLeave={stopIncrement} onClick={handleMinus}>
+			<Button variant="secondary" onClick={handleMinus}>
 				<Minus />
 			</Button>
 			<div className="flex flex-col mb-4 w-full">
@@ -94,7 +79,7 @@ export const RangeWithButtons: FC<IRangeWithButtonsProps> = ({ limits }) => {
 					<RouletteSlider minPrice={limits.min} maxPrice={limits.max} marks={marks} value={activeChipValue} setSliderValue={(amount) => change({ amount })} />
 				</div>
 			</div>
-			<Button variant="secondary" onMouseDown={() => startIncrement('increase')} onMouseUp={stopIncrement} onMouseLeave={stopIncrement} onClick={handlePlus}>
+			<Button variant="secondary" onClick={handlePlus}>
 				<PlusIcon />
 			</Button>
 		</>
