c879d30eda330f6acb2c104fdddb1b0e7b138464
bf-tower <bf.tower@proton.me>
2025-06-29 19:58:20 +0300

    fix: unify all error toasts to use handleError function

--- src/lib/shared/query/index.ts
+++ src/lib/shared/query/index.ts
@@ -4,7 +4,7 @@ import { fetchBetsBitMapAndAmountByRound } from '@/src/lib/shared/gql';
 import { toast } from '@betfinio/components/ui';
 import { type QueryClient, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useParams, useSearch } from '@tanstack/react-router';
-import { getTransactionLink } from 'betfinio_context/lib/helpers';
+import { getTransactionLink, handleError } from 'betfinio_context/lib/helpers';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import type { Address, WriteContractErrorType, WriteContractReturnType } from 'viem';
@@ -180,27 +180,18 @@ export const useSubmitBet = () => {
 	const { t } = useTranslation('roulette');
 	const config = useConfig();
 	const queryClient = useQueryClient();
-	const { isSingle } = useVisibleTable();
 
 	return useMutation<WriteContractReturnType, WriteContractErrorType, SpinParams>({
 		mutationKey: ['roulette', 'spin'],
 		mutationFn: (params) => submitBet(params, config),
 		onError: (e) => {
-			logger.error(e);
-			// @ts-ignore
-			if (e.cause?.reason) {
-				// @ts-ignore
-				if (e.cause.reason === 'LT02' || e.cause.reason === 'LT03') {
-					openPaytable(queryClient);
-				}
-
-				// @ts-ignore
-				toast.error(errors(e.cause?.reason, { defaultValue: t(`errors.${e.cause?.reason}`) }));
-			} else {
-				toast.error(errors('unknown'));
+			if (e.cause.reason === 'LT02' || e.cause.reason === 'LT03') {
+				openPaytable(queryClient);
 			}
+
+			toast.error(handleError(e, errors));
 		},
-		onSuccess: async (data, variables) => {
+		onSuccess: async (data) => {
 			const promise = async () => {
 				const receipt = await waitForTransactionReceipt(config.getClient(), { hash: data });
 				if (receipt.status !== 'success') {
