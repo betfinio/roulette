6f0054ec793a12e98b6978b995e1a0b457adc033
bf-tower <bf.tower@proton.me>
2025-08-28 18:08:57 +0300

    fix: update clipboard copy notification message

--- src/components/live-roulette/WheelDetails/RoundNumber.tsx
+++ src/components/live-roulette/WheelDetails/RoundNumber.tsx
@@ -1,18 +1,19 @@
 import { toast } from '@betfinio/components/ui';
 import { CheckIcon, CopyIcon } from 'lucide-react';
-import { type FC, useState } from 'react';
+import { type FC, useId, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useGetSelectedRound } from '@/src/lib/live-roulette/query';
 
 export const RoundNumber: FC = () => {
 	const { t } = useTranslation('roulette');
+	const textId = useId();
 
 	const { round } = useGetSelectedRound();
 	const [addressCopied, setAddressCopied] = useState(false);
 
 	const handleCopyRoundAddress = async () => {
-		toast.success(t('copiedCurrentRoundRef'));
 		await navigator.clipboard.writeText(location.href);
+		toast.info(t('copiedCurrentRoundRef'));
 		setAddressCopied(true);
 		setTimeout(() => {
 			setAddressCopied(false);
@@ -22,7 +23,7 @@ export const RoundNumber: FC = () => {
 	return (
 		<>
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110 20" className="w-full">
-				<text id="timerText" x="50%" y="50%" textAnchor="middle" className="fill-muted/50 text-xs" dy=".3em">
+				<text id={textId} x="50%" y="50%" textAnchor="middle" className="fill-muted/50 text-xs" dy=".3em">
 					{t('round')} #{round}
 				</text>
 			</svg>
